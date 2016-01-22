aeee1f7a0934387d43ed50779d9fe62564ed2526
Pierrick Koch <pierrick.koch@gmail.com>
2016-01-22 16:25:59 +0100

    [js] add rainbow colormap in ShaderTerrain

--- js/ShaderTerrain.js
+++ js/ShaderTerrain.js
@@ -116,6 +116,45 @@ THREE.ShaderTerrain = {
 			THREE.ShaderChunk[ "shadowmap_pars_fragment" ],
 			THREE.ShaderChunk[ "fog_pars_fragment" ],
 
+			// rainbow colormap, TODO add Viridis from bids.github.io/colormap
+			"vec4 colormap(float x) {",
+				"float r = 0.0, g = 0.0, b = 0.0;",
+
+				"if (x < 0.0) {",
+					"r = 127.0 / 255.0;",
+				"} else if (x <= 1.0 / 9.0) {",
+					"r = 1147.5 * (1.0 / 9.0 - x) / 255.0;",
+				"} else if (x <= 5.0 / 9.0) {",
+					"r = 0.0;",
+				"} else if (x <= 7.0 / 9.0) {",
+					"r = 1147.5 * (x - 5.0 / 9.0) / 255.0;",
+				"} else {",
+					"r = 1.0;",
+				"}",
+
+				"if (x <= 1.0 / 9.0) {",
+					"g = 0.0;",
+				"} else if (x <= 3.0 / 9.0) {",
+					"g = 1147.5 * (x - 1.0 / 9.0) / 255.0;",
+				"} else if (x <= 7.0 / 9.0) {",
+					"g = 1.0;",
+				"} else if (x <= 1.0) {",
+					"g = 1.0 - 1147.5 * (x - 7.0 / 9.0) / 255.0;",
+				"} else {",
+					"g = 0.0;",
+				"}",
+
+				"if (x <= 3.0 / 9.0) {",
+					"b = 1.0;",
+				"} else if (x <= 5.0 / 9.0) {",
+					"b = 1.0 - 1147.5 * (x - 3.0 / 9.0) / 255.0;",
+				"} else {",
+					"b = 0.0;",
+				"}",
+
+				"return vec4(r, g, b, 1.0);",
+			"}",
+
 			"void main() {",
 
 				"gl_FragColor = vec4( vec3( 1.0 ), opacity );",
@@ -150,6 +189,11 @@ THREE.ShaderTerrain = {
 				"} else if( enableDiffuse2 ) {",
 
 					"gl_FragColor = gl_FragColor * texture2D( tDiffuse2, uvOverlay );",
+
+				"} else {",
+
+					//"gl_FragColor = gl_FragColor * texture2D( tDisplacement, uvBase );",
+					"gl_FragColor = gl_FragColor * colormap( texture2D( tDisplacement, uvBase ).x );",
 
 				"}",
 
