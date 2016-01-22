7a6446257ee79d2b86c846f25e747c5821e7bce4
Pierrick Koch <pierrick.koch@gmail.com>
2016-01-22 17:57:33 +0100

    [js] replace rainbow colormap by summer

--- js/ShaderTerrain.js
+++ js/ShaderTerrain.js
@@ -116,43 +116,9 @@ THREE.ShaderTerrain = {
 			THREE.ShaderChunk[ "shadowmap_pars_fragment" ],
 			THREE.ShaderChunk[ "fog_pars_fragment" ],
 
-			// rainbow colormap, TODO add Viridis from bids.github.io/colormap
+			// summer colormap, TODO add Viridis from bids.github.io/colormap
 			"vec4 colormap(float x) {",
-				"float r = 0.0, g = 0.0, b = 0.0;",
-
-				"if (x < 0.0) {",
-					"r = 127.0 / 255.0;",
-				"} else if (x <= 1.0 / 9.0) {",
-					"r = 1147.5 * (1.0 / 9.0 - x) / 255.0;",
-				"} else if (x <= 5.0 / 9.0) {",
-					"r = 0.0;",
-				"} else if (x <= 7.0 / 9.0) {",
-					"r = 1147.5 * (x - 5.0 / 9.0) / 255.0;",
-				"} else {",
-					"r = 1.0;",
-				"}",
-
-				"if (x <= 1.0 / 9.0) {",
-					"g = 0.0;",
-				"} else if (x <= 3.0 / 9.0) {",
-					"g = 1147.5 * (x - 1.0 / 9.0) / 255.0;",
-				"} else if (x <= 7.0 / 9.0) {",
-					"g = 1.0;",
-				"} else if (x <= 1.0) {",
-					"g = 1.0 - 1147.5 * (x - 7.0 / 9.0) / 255.0;",
-				"} else {",
-					"g = 0.0;",
-				"}",
-
-				"if (x <= 3.0 / 9.0) {",
-					"b = 1.0;",
-				"} else if (x <= 5.0 / 9.0) {",
-					"b = 1.0 - 1147.5 * (x - 3.0 / 9.0) / 255.0;",
-				"} else {",
-					"b = 0.0;",
-				"}",
-
-				"return vec4(r, g, b, 1.0);",
+				"return vec4(clamp(x, 0.0, 1.0), clamp(0.5 * x + 0.5, 0.0, 1.0), 0.4, 1.0);",
 			"}",
 
 			"void main() {",
